97053d4c5551faa08e4ef3d7f07b7d5c6fa14727
AngelVasilev <angavasilev@gmail.com>
2022-12-03 20:27:23 +0200

    Create .gitconfig.sh

---
+++ .gitconfig.sh
@@ -0,0 +1,8 @@
+[alias]
+  ch = checkout
+  c = commit
+  st = status
+  br = branch
+  logwh = log --pretty=format:'%h %ad | %s%d [%an]' --graph --date=short
+  type = cat-file -t
+  dump = cat-file -p
