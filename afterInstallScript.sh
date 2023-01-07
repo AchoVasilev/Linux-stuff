3bfdad209e8bb629d0d94057dd75e45524c9318f
AngelVasilev <angavasilev@gmail.com>
2023-01-07 10:41:32 +0200

    Update afterInstallScript.sh

--- afterInstallScript.sh
+++ afterInstallScript.sh
@@ -1,15 +1,53 @@
 #!/bin/bash
 
-#Update the system
+#configure dnf
+sudo nano /etc/dnf/dnf.conf
+max_parallel_downloads=10
+fastestmirror=true
+deltarpm=true
+
+#update system
 sudo dnf update -y
 
-#Add programs
-mkdir Programs
-cd Programs
+#add rpm repos
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm -y
+sudo dnf install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 
-##Install IntelliJ Community
+#add flatpak
+sudo flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo -y
+
+#install tweaks
+sudo dnf install gnome-tweaks 
+flatpak install flathub com.mattjakeman.ExtensionManager
+
+#install multimedia
+sudo dnf install gstreamer1-plugins-{bad-\*,good-\*,base} gstreamer1-plugin-openh264 gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel -y
+sudo dnf install lame\* --exclude=lame-devel -y
+sudo dnf group upgrade --with-optional Multimedia -y
+sudo dnf install vlc
+
+#create programming directory
+mkdir Programs && cd Programs
+
+#Install postman
+sudo flatpak install postman
+
+#Install Visual Studio Code
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+dnf check-update
+sudo dnf install code -y
+
+#Install docker
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager \
+    --add-repo \
+    https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+wget https://desktop.docker.com/linux/main/amd64/docker-desktop-4.15.0-x86_64.rpm?utm_source=docker&utm_medium=webreferral&utm_campaign=docs-driven-download-linux-amd64
+sudo dnf install docker-desktop-4.15.0-x86_64.rpm
+
+#Install IntelliJ
 wget https://download.jetbrains.com/idea/ideaIC-2022.3.tar.gz
 sudo tar -xzf ideaIC-2022.3.tar.gz --directory=/home/acho/Programs
-
-
-
+sudo ln -sf /home/acho/Programs/idea/bin/idea.sh /bin/intellijidea-ce
