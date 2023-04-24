7a73109b684528d9ba867923cb65c63f72dcddf8
AngelVasilev <angavasilev@gmail.com>
2023-04-24 10:14:05 +0300

    Update ColorTerminalLikeGit.sh

--- ColorTerminalLikeGit.sh
+++ ColorTerminalLikeGit.sh
@@ -63,6 +63,13 @@ else
 fi
 unset color_prompt force_color_prompt
 
+#Uncomment to use it on single line.
+# if [ "$color_prompt" = yes ]; then
+ # PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[01;31m\] $(parse_git_branch)\[\033[00m\]\$ '
+# else
+ # PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w$(parse_git_branch)\$ '
+# fi
+
 # If this is an xterm set the title to user@host:dir
 case "$TERM" in
 xterm*|rxvt*)
